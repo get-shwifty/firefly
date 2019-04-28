eb2cd292a42698796058fa83a84e0308e5a34400
Alexis Schad <schadoc_alex@hotmail.fr>
2019-04-28 14:44:04 +0200

    new level structure

--- js/core/LevelManager.js
+++ js/core/LevelManager.js
@@ -3,21 +3,34 @@ import { Black } from "black-engine";
 import level1_1 from 'assets/levels/level1_1.json'
 
 export default class LevelManager {
-  constructor() {
-    this.levels = [{
-      name: 'level1_1',
-      file: level1_1
-    }]
-    this.current = 0
-  }
-
-  enqueueLevels(assets) {
-    for(const level of this.levels) {
-      assets.enqueueJSON(level.name, level.file)
+    constructor() {
+        this.levels = [{
+            name: 'level1_1',
+            file: level1_1,
+            obj: null
+        }]
+        this.current = 0
     }
-  }
 
-  get level() {
-    return Black.assets.getJSON(this.levels[this.current].name)
-  }
+    enqueueLevels(assets) {
+        for(const level of this.levels) {
+            assets.enqueueJSON(level.name, level.file)
+        }
+    }
+
+    onAssetsLoadded() {
+        for(const level of this.levels) {
+            level.obj = Black.assets.getJSON(level.name)
+            for(const x in Object.keys(level.obj.world)) {
+                for(const y in Object.keys(level.obj.world[x])) {
+                    const obj = level.obj.world[x][y]
+                    obj.id = 'world_' + x + '_' + y
+                }
+            }
+        }
+    }
+
+    get level() {
+        return this.levels[this.current].obj
+    }
 }
--- js/game.js
+++ js/game.js
@@ -6,55 +6,56 @@ import { GameObject, AssetManager } from 'black-engine';
 const TILE_SIZE = 100
 
 export class Game extends GameObject {
-  constructor() {
-    super()
+    constructor() {
+        super()
 
-    this.levelManager = new LevelManager()
-    this.displayManager = new DisplayManager()
+        this.levelManager = new LevelManager()
+        this.displayManager = new DisplayManager()
 
-    const assets = new AssetManager()
+        const assets = new AssetManager()
 
-    this.levelManager.enqueueLevels(assets)
+        this.levelManager.enqueueLevels(assets)
 
-    assets.on('complete', this.onAssetsLoadded, this)
-    assets.loadQueue()
-  }
-
-  onAssetsLoadded(m) {
-    this.onNewLevel()
-  }
-
-  onKeyPress(msg, keyInfo) {
-    let action;
-    switch (keyInfo.keyCode) {
-      case Key.UP_ARROW:
-        action = actions.UP
-        break
-      case Key.DOWN_ARROW:
-        action = actions.DOWN
-        break
-      case Key.LEFT_ARROW:
-        action = actions.LEFT
-        break
-      case Key.RIGHT_ARROW:
-        action = actions.RIGHT
-        break
-      case Key.SPACE:
-        action = actions.SWAP
-        break
+        assets.on('complete', this.onAssetsLoadded, this)
+        assets.loadQueue()
     }
 
-    if(action) {
-      const diff = gameLoop(state, action)
-      this.displayManager.updateLevel(diff)
+    onAssetsLoadded(m) {
+        this.levelManager.onAssetsLoadded()
+        this.onNewLevel()
     }
-  }
 
-  onNewLevel() {
-    this.displayManager.createLevel(this.levelManager.level)
-  }
+    onKeyPress(msg, keyInfo) {
+        let action;
+        switch (keyInfo.keyCode) {
+            case Key.UP_ARROW:
+            action = actions.UP
+            break
+            case Key.DOWN_ARROW:
+            action = actions.DOWN
+            break
+            case Key.LEFT_ARROW:
+            action = actions.LEFT
+            break
+            case Key.RIGHT_ARROW:
+            action = actions.RIGHT
+            break
+            case Key.SPACE:
+            action = actions.SWAP
+            break
+        }
+
+        if(action) {
+            const diff = gameLoop(state, action)
+            this.displayManager.updateLevel(diff)
+        }
+    }
+
+    onNewLevel() {
+        this.displayManager.createLevel(this.levelManager.level)
+    }
 }
 
 export {
-  TILE_SIZE
+    TILE_SIZE
 }
