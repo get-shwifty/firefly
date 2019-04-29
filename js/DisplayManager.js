6630a05b643cbf0616517160bedf39147cd15d67
feldvale <feldvale@utc.fr>
2019-04-29 12:35:30 +0200

    display : new objects, godray still missing

--- js/DisplayManager.js
+++ js/DisplayManager.js
@@ -1,21 +1,23 @@
 import { GameObject } from "black-engine";
 import { Tile } from "./engine"
-import Firefly from './firefly'
-import Bat from './bat'
-import Sunflower from './sunflower'
-import Ground from './ground'
-import Door from './door'
+import Firefly from './display/firefly'
+import Bat from './display/bat'
+import Sunflower from './display/sunflower'
+import Ground from './display/ground'
+import Door from './display/door'
+import Spike from './display/spike'
+import Crystal from './display/crystal'
 import {TILE_SIZE} from './game.js'
 
 
 // To Update : when the tiles classes will be defined
 const TILE_CLASS = {
-    [Tile.GROUND]: GameObject,
+    [Tile.GROUND]: Ground,
     [Tile.BAT]: Bat,
     [Tile.SUNFLOWER]: Sunflower,
-    [Tile.CRYSTAL]: GameObject,
-    [Tile.DOOR]: GameObject,
-    [Tile.SPIKE]: GameObject
+    [Tile.CRYSTAL]: Crystal,
+    [Tile.DOOR]: Door,
+    [Tile.SPIKE]: Spike
 }
 
 export default class DisplayManager extends GameObject {
@@ -37,26 +39,25 @@ export default class DisplayManager extends GameObject {
         console.log(level)
         this.worldGameObjects = {}
 
-        // Creating Player
-        this.player = this.addChild(new Firefly())
-        this.player.updatePosition(level.player.pos.x * TILE_SIZE, level.player.pos.y * TILE_SIZE)
-        this.player.update(level.player)
-
         // Creating environment
         for(const x of Object.keys(level.world)) {
             for(const y of Object.keys(level.world[x])) {
                 const CLASS = TILE_CLASS[level.world[x][y].type]
                 if (CLASS){
-                const newTile = this.addChild(new CLASS())
-                newTile.x = +x 
-                newTile.y = Number(y)
+                const newTile = this.addChild(new CLASS(level.world[x][y]))
+                newTile.x = +x * TILE_SIZE
+                newTile.y = +y * TILE_SIZE
                 this.worldGameObjects[level.world[x][y].id] = newTile
                 } else {
                     throw Error("Error : world element" + " '" + level.world[x][y].type + "' not recognized")
                 }
             }
         }
-        // console.log(this.worldGameObjects)
+
+        // Creating Player (After environment for layering. Check Z-index Alexis ?)
+        this.player = this.addChild(new Firefly())
+        this.player.updatePosition(level.player.pos.x * TILE_SIZE, level.player.pos.y * TILE_SIZE)
+        this.player.update(level.player)
     }
 
     updateLevel(diff) {
