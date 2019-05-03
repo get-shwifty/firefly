ab243f1a7cd7c6c4625b907aa6699509a8b552f0
Félix <someonefefe@gmail.com>
2019-05-04 00:14:47 +0200

    setting emitters as childrent from fireflies sprites

--- js/display/firefly.js
+++ js/display/firefly.js
@@ -1,24 +1,32 @@
-import { GameObject, Black, AssetManager, Sprite, VectorScatter, InitialVelocity, Acceleration, ScaleOverLife, BlendMode, AnimationController, Graphics, Tween, Ease, Emitter, FloatScatter, InitialLife  } from 'black-engine'
-
-import {TILE_SIZE} from '../game'
+import { GameObject, Black, AssetManager, Sprite, VectorScatter, InitialVelocity, Acceleration, ScaleOverLife, BlendMode, AnimationController, Graphics, Tween, Ease, Emitter, FloatScatter, InitialLife } from 'black-engine'
+import _ from 'lodash'
+import { TILE_SIZE } from '../game'
 const POS = [
-        { x: 0.5, y: 0.5},
-        { x: 0.20, y: 0.20},
-        { x: 0.80, y: 0.20},
-        { x: 0.80, y: 0.80},
-        { x: 0.20, y: 0.80},
-    ]
+  { x: 0.5, y: 0.5 },
+  { x: 0.20, y: 0.20 },
+  { x: 0.80, y: 0.20 },
+  { x: 0.80, y: 0.80 },
+  { x: 0.20, y: 0.80 },
+]
 const speeds = [1.5, 1, 2, 0.5, 0.3]
 export default class Firefly extends GameObject {
   constructor() {
     super();
-    this.life = 5
+    this.MAX_LIFE = 5
+    this.life = this.MAX_LIFE
     this.glow = 3
-    this.move = true
   }
 
   //Mouvement de la libellule
-  updatePosition(x,y){
+  updatePosition(x, y) {
+    for (let i in this.fireflies) {
+      const offsetX = x - this.x;
+      const offsetY = y - this.y;
+      console.log(i, offsetX, offsetY)
+      const [newPosX, newPosY] = this.sinusPosition(i, 0, offsetX, offsetY)
+      let tween = new Tween({ x: newPosX, y: newPosY }, 1, { playOnAdded: false, ease: Ease.backIn })
+      this.fireflies[i].addComponent(tween);
+    }
     this.x = x;
     this.y = y;
   }
@@ -26,62 +34,62 @@ export default class Firefly extends GameObject {
   //Swap de la vie et de la lumière
   update(player) {
     this.life = player.life
-    this.glow = player.glow 
+    this.glow = player.glow
     //Affichage ou suppression des libellules en fonction de la vie 
-    for (let i = 0; i < 5; i++){
-      if(player.swap){
-        const dt = Black.time.now + Black.time.dt * 60 * 0.6
-        const dir = i%2 == 0 ? 1 : -1
-        const newPosX = dir * Math.cos(dt * speeds[i] + POS[i].x) * TILE_SIZE / 3 + TILE_SIZE / 2
-        const newPosY = dir * Math.sin(2*dt * speeds[i] + POS[i].y) * TILE_SIZE / 3 + TILE_SIZE / 2
-        let tween1 = new Tween({ x: TILE_SIZE / 2, y: TILE_SIZE / 2}, 0.3, {ease: Ease.backOut})
-        let tween2 = new Tween({ x: newPosX, y: newPosY }, 0.3,{playOnAdded: false, ease: Ease.backIn})
+    for (let i = 0; i < this.MAX_LIFE; i++) {
+      if (player.swap) {
+        const [newPosX, newPosY] = this.sinusPosition(i, Black.time.dt * 60 * 0.6)
+        let tween1 = new Tween({ x: TILE_SIZE / 2, y: TILE_SIZE / 2 }, 0.3, { ease: Ease.backOut })
+        let tween2 = new Tween({ x: newPosX, y: newPosY }, 0.3, { playOnAdded: false, ease: Ease.backIn })
         tween1.chain(tween2)
         this.fireflies[i].addComponent(tween1);
         this.fireflies[i].addComponent(tween2);
       }
-      this.emitters[i].addModifier(new ScaleOverLife(new FloatScatter(0.05 * this.glow, 0, Ease.backIn)),)
+      this.emitters[i].addModifier(new ScaleOverLife(new FloatScatter(0.05 * this.glow, 0, Ease.backIn)))
       this.fireflies[i].visible = i < this.life;
       this.emitters[i].alpha = i < this.life;
     }
-    this.move = true
- 
+  }
+
+  sinusPosition(i, dtIncrement = 0, offsetX = 0, offsetY = 0) {
+    const dt = Black.time.now + dtIncrement
+    const dir = i % 2 == 0 ? 1 : -1
+    const x = dir * Math.cos(dt * speeds[i] + POS[i].x) * TILE_SIZE / 3 + TILE_SIZE / 2 + offsetX
+    const y = dir * Math.sin(2 * dt * speeds[i] + POS[i].y) * TILE_SIZE / 3 + TILE_SIZE / 2 + offsetY
+    return [x, y]
   }
 
   onAdded(m) {
     this.fireflies = []
     this.emitters = []
-    for (let i = 0; i < 5; i++){
-        this.fireflies.push(this.createAnimation(TILE_SIZE*POS[i].x, TILE_SIZE*POS[i].y))
-        this.emitters.push(this.createEmitter())
+    for (let i = 0; i < this.MAX_LIFE; i++) {
+      this.fireflies.push(this.createAnimation(TILE_SIZE * POS[i].x, TILE_SIZE * POS[i].y))
+      let e = this.createEmitter(i)
+      this.emitters.push(e)
+      this.fireflies[i].addChild(e)
+      this.fireflies[i].alignPivotOffset()
     }
   }
 
-  onUpdate(){
-    if(this.fireflies.length == 5 && this.emitters.length == 5)
-    {
-      const dt =  Black.time.now
-      this.updateFireflies(dt)
+  onUpdate() {
+    if (this.fireflies.length == this.MAX_LIFE && this.emitters.length == this.MAX_LIFE) {
+      this.updateFireflies()
     }
     else return
   }
 
-  updateFireflies(dt){
+  updateFireflies() {
     this.fireflies.forEach((_, i) => {
-      const dir = i%2 == 0 ? 1 : -1
-      const newPosX = dir * Math.cos(dt * speeds[i] + POS[i].x) * TILE_SIZE / 3 + TILE_SIZE / 2
-      const newPosY = dir * Math.sin(2*dt * speeds[i] + POS[i].y) * TILE_SIZE / 3 + TILE_SIZE / 2
+      const [newPosX, newPosY] = this.sinusPosition(i)
       this.fireflies[i].x = newPosX
       this.fireflies[i].y = newPosY
-      this.emitters[i].x = newPosX
-      this.emitters[i].y = newPosY
     })
   }
 
-  createEmitter(){
+  createEmitter(i) {
     let texture = Black.assets.getTextures('particle_firefly');
     let emitter = new Emitter();
-     // Zero all default values since we dont need any particles at the start
+    // Zero all default values since we dont need any particles at the start
     emitter.emitCount = new FloatScatter(0.1);
     emitter.emitDelay = new FloatScatter(0);
     emitter.emitInterval = new FloatScatter(0);
@@ -100,12 +108,11 @@ export default class Firefly extends GameObject {
       // Make particles small over life
       new ScaleOverLife(new FloatScatter(0.02 * this.glow, 0, Ease.backIn)),
 
-      // Give some acceleration is all directions
+      // Give some acceleration in all directions
       new Acceleration(new VectorScatter(-20, -20, 20, 20)),
     );
- 
+
     // Add to scene
-    this.addChild(emitter);
     return emitter
   }
 
@@ -124,14 +131,8 @@ export default class Firefly extends GameObject {
     this.anim.add('anim', textureAnim, 50);
     this.anim.play('anim');
 
-    sprite.alignPivot();
+    // sprite.alignPivotOffset();
     sprite.scale = 0.3
-
-    // //Rotation des lucioles
-    // let tween = new Tween({ rotation: 2 * Math.PI }, 4, { ease: Ease.linear });
-    // // Set loop to true
-    // tween.loop = true;
-    // sprite.addComponent(sprite);
 
     this.addChild(sprite);
 
